41765bd629af6db0ee848a430f9d5fbe9b9c73ec
ali <a_icam@yahoo.com>
2021-11-14 12:33:40 +0330

    airdrop.io finished without show more option

--- airdrops_io.py
+++ airdrops_io.py
@@ -5,6 +5,10 @@ req = requests.get(url)
 
 
 soup = BeautifulSoup(req.text, "html.parser")
-show_more = soup.find('div', {'class': 'airdrops-shortcoded airdrop-hover'})
-print(show_more)
-# print(soup.find_all('div',{'class': 'inside-article'})[0])
+all_articles = soup.find('div', {'class': 'airdrops-shortcoded airdrop-hover'})
+for article in all_articles.find_all('article'):
+    print(article.find('h3').contents[0])
+    print(article.find('div', {'class': 'inside-article'})['onclick'].split("'")[1].split("'")[0])
+    span_val = article.find_all('span')[0] if len(article.find_all('span')) == 1 else article.find_all('span')[1]
+    print(span_val.contents[0])
+    print('----------')
